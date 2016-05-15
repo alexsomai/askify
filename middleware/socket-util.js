0eb574519555d09f79460941d57c8949417a75ae
Alexandru Somai <alex_somai@yahoo.com>
2016-05-16 01:13:52 +0300

    Configure socket.io client side to work with jwt.

--- middleware/socket-util.js
+++ middleware/socket-util.js
@@ -5,25 +5,30 @@ const socket = io('//localhost:3001')
 let subscribtion
 
 export function subscribe(props) {
-  const { questions, actions, room } = props
+  socket.on('connect', () => {
+  	socket.on('authenticated', () => {
+      const { questions, actions, room } = props
 
-  const createStream$ = Rx.Observable
-    .fromEvent(socket, 'question:create')
-    .filter(item => item.room === room)
-  const voteStream$ = Rx.Observable
-    .fromEvent(socket, 'question:vote')
-    .filter(item => item.room === room)
-  const doneStream$ = Rx.Observable
-    .fromEvent(socket, 'question:done')
-    .filter(item => item.room === room)
+      const createStream$ = Rx.Observable
+        .fromEvent(socket, 'question:create')
+        .filter(item => item.room === room)
+      const voteStream$ = Rx.Observable
+        .fromEvent(socket, 'question:vote')
+        .filter(item => item.room === room)
+      const doneStream$ = Rx.Observable
+        .fromEvent(socket, 'question:done')
+        .filter(item => item.room === room)
 
-  const action$ = Rx.Observable.merge(
-    createStream$.map(actions.addQuestion),
-    voteStream$.map(actions.voteQuestion),
-    doneStream$.map(actions.doneQuestion)
-  )
+      const action$ = Rx.Observable.merge(
+        createStream$.map(actions.addQuestion),
+        voteStream$.map(actions.voteQuestion),
+        doneStream$.map(actions.doneQuestion)
+      )
 
-  if (typeof subscribtion === 'undefined') {
-    subscribtion = action$.subscribe(questions.dispatch)
-  }
+      if (typeof subscribtion === 'undefined') {
+        subscribtion = action$.subscribe(questions.dispatch)
+      }
+  	})
+  	.emit('authenticate', { token: localStorage.getItem('id_token') }) // send the jwt
+    })
 }
