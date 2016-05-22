bb70de928b925a71728334951a26f7eb83699225
Alexandru Somai <somai.alexandru@gmail.com>
2016-05-22 14:31:46 +0300

    Fix socket.io subscription bug.

--- middleware/socket-util.js
+++ middleware/socket-util.js
@@ -1,34 +1,31 @@
 import Rx from 'rx'
 import io from 'socket.io-client'
 
-const socket = io('//localhost:3001')
 let subscribtion
+const socket = io('//localhost:3001')
+socket.on('connect', () => {})
+  .emit('authenticate', { token: localStorage.getItem('id_token') }) // send the jwt
 
 export function subscribe(props) {
-  socket.on('connect', () => {
-  	socket.on('authenticated', () => {
-      const { questions, actions, room } = props
+  const { questions, actions, room } = props
 
-      const createStream$ = Rx.Observable
-        .fromEvent(socket, 'question:create')
-        .filter(item => item.room === room)
-      const voteStream$ = Rx.Observable
-        .fromEvent(socket, 'question:vote')
-        .filter(item => item.room === room)
-      const doneStream$ = Rx.Observable
-        .fromEvent(socket, 'question:done')
-        .filter(item => item.room === room)
+  const createStream$ = Rx.Observable
+    .fromEvent(socket, 'question:create')
+    .filter(item => item.room === room)
+  const voteStream$ = Rx.Observable
+    .fromEvent(socket, 'question:vote')
+    .filter(item => item.room === room)
+  const doneStream$ = Rx.Observable
+    .fromEvent(socket, 'question:done')
+    .filter(item => item.room === room)
 
-      const action$ = Rx.Observable.merge(
-        createStream$.map(actions.addQuestion),
-        voteStream$.map(actions.voteQuestion),
-        doneStream$.map(actions.doneQuestion)
-      )
+  const action$ = Rx.Observable.merge(
+    createStream$.map(actions.addQuestion),
+    voteStream$.map(actions.voteQuestion),
+    doneStream$.map(actions.doneQuestion)
+  )
 
-      if (typeof subscribtion === 'undefined') {
-        subscribtion = action$.subscribe(questions.dispatch)
-      }
-  	})
-  	.emit('authenticate', { token: localStorage.getItem('id_token') }) // send the jwt
-    })
+  if (typeof subscribtion === 'undefined') {
+    subscribtion = action$.subscribe(questions.dispatch)
+  }
 }
