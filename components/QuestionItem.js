cb9aeaac9f2f5319f44f67a36908cb4a76d53c85
Alexandru Somai <somai.alexandru@gmail.com>
2019-07-16 13:49:09 +0300

    Update QuestionItem component to use @material-ui/core

--- components/QuestionItem.js
+++ components/QuestionItem.js
@@ -1,8 +1,15 @@
 import React, { Component } from 'react'
 import PropTypes from 'prop-types'
-
-import { Card, CardHeader, CardText } from 'material-ui/Card'
-import IconButton from 'material-ui/IconButton'
+import Card from '@material-ui/core/Card';
+import CardHeader from '@material-ui/core/CardHeader';
+import CardContent from '@material-ui/core/CardContent';
+import Avatar from '@material-ui/core/Avatar';
+import IconButton from '@material-ui/core/IconButton';
+import Typography from '@material-ui/core/Typography';
+import Tooltip from '@material-ui/core/Tooltip';
+import ThumbUp from '@material-ui/icons/ThumbUp';
+import Done from '@material-ui/icons/Done';
+import Cancel from '@material-ui/icons/Cancel';
 
 const style = {
   container: {
@@ -22,13 +29,16 @@ const style = {
   text: {
     wordWrap: 'break-word',
     fontSize: 16
+  },
+  avatar: {
+    backgroundColor: '#b8b8b8'
   }
-}
+};
 
 export default class QuestionItem extends Component {
   constructor(props) {
-    super(props)
-    this.handleVote = this.handleVote.bind(this)
+    super(props);
+    this.handleVote = this.handleVote.bind(this);
     this.handleDone = this.handleDone.bind(this)
   }
 
@@ -41,38 +51,46 @@ export default class QuestionItem extends Component {
   }
 
   render() {
-    const { question, thumbUpDisabled, doneDisabled } = this.props
-    const votes = `${question.votes} votes`
+    const { question, thumbUpDisabled, doneDisabled } = this.props;
+    const votes = `${question.votes} votes`;
     const doneQuestion = question.done
-      ? { style: style.doneQuestion, tooltip: 'Mark question as unanswered', icon: 'cancel' }
-      : { style: style.undoneQuestion, tooltip: 'Mark question as answered', icon: 'done'}
+      ? { style: style.doneQuestion, tooltip: 'Mark question as unanswered', icon: <Cancel /> }
+      : { style: style.undoneQuestion, tooltip: 'Mark question as answered', icon: <Done /> };
+
     return (
-      <Card style={style.container} containerStyle={doneQuestion.style}>
-        <CardHeader style={style.header}
-          title={question.username}
-          subtitle={votes}
-          avatar={question.picture}>
-            <div style={{float:'right'}}>
-              <IconButton
-                iconClassName="material-icons"
-                onClick={this.handleVote}
-                disabled={thumbUpDisabled}
-                tooltip="Vote question">
-                thumb_up
-              </IconButton>
-              <IconButton
-                iconClassName="material-icons"
-                onClick={this.handleDone}
-                disabled={doneDisabled}
-                tooltip={doneQuestion.tooltip}>
-                {doneQuestion.icon}
-              </IconButton>
-            </div>
-        </CardHeader>
-        <CardText style={style.text}>
-          {question.text}
-        </CardText>
-      </Card>
+      <div style={doneQuestion.style}>
+        <Card style={style.container}>
+          <CardHeader
+            avatar={<Avatar src={question.picture} style={style.avatar} />}
+            style={style.header}
+            title={question.username}
+            subheader={votes}
+            action={
+              <div>
+                <Tooltip title="Vote question">
+                  <div style={{ float: 'left' }}>
+                    <IconButton onClick={this.handleVote} disabled={thumbUpDisabled}>
+                      <ThumbUp />
+                    </IconButton>
+                  </div>
+                </Tooltip>
+                <Tooltip title={doneQuestion.tooltip}>
+                  <div style={{ float: 'left', marginRight: '25px' }}>
+                    <IconButton onClick={this.handleDone} disabled={doneDisabled}>
+                      {doneQuestion.icon}
+                    </IconButton>
+                  </div>
+                </Tooltip>
+              </div>
+            }
+          />
+          <CardContent>
+            <Typography style={style.text}>
+              {question.text}
+            </Typography>
+          </CardContent>
+        </Card>
+      </div>
     )
   }
 }
@@ -83,4 +101,4 @@ QuestionItem.propTypes = {
   onDone: PropTypes.func.isRequired,
   thumbUpDisabled: PropTypes.bool.isRequired,
   doneDisabled: PropTypes.bool.isRequired
-}
+};
